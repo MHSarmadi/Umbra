0b0ef5e93f0d54a31642a18208dc1383ff89c212
MHSarmadi <Programming.MHSarmadi@gmail.com>
2025-08-16 00:56:37 +0330

    Add current version to contexts + more documentations

--- Server/crypto/mac.go
+++ Server/crypto/mac.go
@@ -4,7 +4,7 @@ import "github.com/zeebo/blake3"
 
 func MAC(key, data []byte, context string) (digest [32]byte) {
 	safeKey := make([]byte, 32)
-	blake3.DeriveKey("@UMBRA-@STDMAC-"+context, key, safeKey)
+	blake3.DeriveKey("@UMBRAv0.0.0-@STDMAC-"+context, key, safeKey)
 	h, _ := blake3.NewKeyed(safeKey)
 	h.Write(data)
 	h.Digest().Read(digest[:])
